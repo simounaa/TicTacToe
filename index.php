7f67057e1bfb9625ff7bf389a7966fbed348a1f6
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-11-08 13:21:22 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <div class="game">
+            <span style="--i:11"></span>
+            <span style="--i:32"></span>
+            <span style="--i:43"></span>
+            <span style="--i:12"></span>
+            <span style="--i:23"></span>
+            <span style="--i:34"></span>
+            <span style="--i:27"></span>
+            <span style="--i:15"></span>
+            <span style="--i:26"></span>
+            <span style="--i:17"></span>
+            <span style="--i:18"></span>
+            <span style="--i:29"></span>
+            <span style="--i:20"></span>
+            <span style="--i:22"></span>
+            <span style="--i:25"></span>
+            <span style="--i:15"></span>
+            <span style="--i:28"></span>
+            <img src="images/XO.png" alt="" class="bg-menu">
+            <button class="start" onclick="start()">START</button>
+            <button class="connect" onclick="connect()">CONNECT</button>
+        </div>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,10 @@
+const pc = new RTCPeerConnection();
+const ws = new WebSocket("ws://localhost:8080");
+
+const start = () => {
+
+}
+
+const connect = () => {
+
+}
